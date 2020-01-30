87a5097d9b7e33cca0cc8f01332f39c075c6a1cc
gjuhro <jure.plavcic@gmail.com>
2020-01-30 15:11:00 +0100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader',
+                ],
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader',
+                ],
+            },
+        ],
+    },
+};
